9294d8a2624595937b6d36f53010fb15d40a9683
kuramocheez <kuramochizento26@gmail.com>
2025-02-12 03:16:24 +0800

    fixed: cannot seed database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,7 +14,48 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        DB::table('sales')->insert([
+            [
+                'customer_name' => 'Khairul Insan Karim',
+                'product_name' => 'Laptop',
+                'qty' => 1,
+                'total_ammount' => 10000000,
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'customer_name' => 'Asep',
+                'product_name' => 'Android TV',
+                'qty' => 5,
+                'total_ammount' => 8000000,
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'customer_name' => 'Ika',
+                'product_name' => 'Iphone 12 Pro Max',
+                'qty' => 1,
+                'total_ammount' => 12500000,
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'customer_name' => 'Marlo',
+                'product_name' => 'Canon 500D',
+                'qty' => 1,
+                'total_ammount' => 15000000,
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'customer_name' => 'Rudi',
+                'product_name' => 'Mouse Gaming',
+                'qty' => 1,
+                'total_ammount' => 200000,
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+        ]);
 
         User::factory()->create([
             'name' => 'Test User',
